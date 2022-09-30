b1d3bf3d3337ff1f5efd42288113fc25184755c2
Afroman726 <dymitrihawkins@gmail.com>
2022-09-29 17:20:00 -0700

    Added some Notes for readers

--- Main.js
+++ Main.js
@@ -2,19 +2,23 @@ const etchBody = document.querySelector('#etch_Body');
 
 
 
-function makerows (rows, columns) {
+function makerows (size) {
     
-        for(i=0; i < (rows * columns); i++){
+        for(i=0; i < (size * size); i++){    //This part makes the sqr of the container (Legnth * Width)
             let square = document.createElement("div");
              etchBody.appendChild(square).className = "block";
-            square.addEventListener("mouseover", () =>{
+            square.addEventListener("mouseover", () =>{     
                 square.style.backgroundColor= 'black';
-                console.log(square);
+                //console.log(square);
             })
+          // etchBody.style.gridTemplateColumns='1fr';
+           //etchBody.style.gridTemplateRows='1fr';
+           console.log('test');
+
         }
 
 
 
 
 }
-makerows(16,16);
+makerows(16);
