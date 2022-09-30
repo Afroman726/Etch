82f653b0b848403f59801b520f0a8ee2ceee7846
Afroman726 <dymitrihawkins@gmail.com>
2022-09-30 15:07:48 -0700

    Added button functionality

--- Main.js
+++ Main.js
@@ -1,19 +1,18 @@
-const etchBody = document.querySelector('#etch_Body');
-
+const etchBody = document.querySelector('#etch_Body');  //Container for the Grid
+const sizeValue = document.querySelector('#size_Value');
 
 
 function makerows (size) {
-    etchBody.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    etchBody.style.gridTemplateColumns = `repeat(${size}, 1fr)`;  //These two set the css styles to fit block inside grid
     etchBody.style.gridTemplateRows=`repeat(${size}, 1fr)`;
      
-        for(i=0; i < (size * size); i++){    //This part makes the sqr of the container (Legnth * Width)
+        for(i=0; i < (size * size); i++){    //This part makes the sqr of the container (Legnth * Width (duh))
             let square = document.createElement("div");
              etchBody.appendChild(square).className = "block";
             square.addEventListener("mouseover", () =>{     
                 square.style.backgroundColor= 'black';
-                //console.log(square);
-            })
-          
+               })
+          console.log('test');
 
         }
 
@@ -21,4 +20,25 @@ function makerows (size) {
 
 
 }
-makerows(32);
+
+//BELOW IS THE CODE FOR THE BUTTONS IN THE SETTINGS
+
+const smallButton = document.querySelector('#small');
+smallButton.addEventListener('click', () => {
+    makerows(16);
+});
+
+const medButton = document.querySelector('#med');
+medButton.addEventListener('click', () => {
+    makerows(32);
+});
+
+const largeButton = document.querySelector('#large');
+largeButton.addEventListener('click', () =>{
+    makerows(64);
+})
+
+const stupidButton = document.querySelector('#stupid');     //This is the don't click me button lol
+stupidButton.addEventListener('click', () =>{
+    makerows(128);
+})
